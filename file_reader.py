cd66c9ed2d580ed2df11e46897ad98e7ee41acfc
jarsinh <scnuhjx@vip.qq.com>
2021-08-12 00:15:39 +0800

    3 commit

--- file_reader.py
+++ file_reader.py
@@ -4,7 +4,7 @@ with open(filename) as file_object:
     for line in file_object:
         join_pi+=line.strip().replace(' ','')
     print(join_pi)
-    print(len(join_pi))
+    print(f'该文件一共{len(join_pi)}位')
 
 
 
